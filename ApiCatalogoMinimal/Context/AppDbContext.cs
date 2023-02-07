dff404ef8318f05c3f2aa7a348e55dcde816edf1
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-02-07 16:57:20 -0300

    Add project files.

---
+++ ApiCatalogoMinimal/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using ApiCatalogoMinimal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogoMinimal.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome).IsRequired().HasMaxLength(80);
+            mb.Entity<Categoria>().Property(c => c.Descricao).IsRequired().HasMaxLength(150);
+
+
+            //Produto
+            mb.Entity<Produto>().HasKey(p => p.ProdutoId);
+            mb.Entity<Produto>().Property(c => c.Nome).HasMaxLength(80).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Descricao).HasMaxLength(150);
+            mb.Entity<Produto>().Property(c => c.Imagem).HasMaxLength(100);
+
+
+            mb.Entity<Produto>().Property(c => c.Preco).HasPrecision(12, 2);
+
+            //relacionamento
+
+            mb.Entity<Produto>().HasOne<Categoria>(c => c.Categoria).WithMany(p => p.Produtos).HasForeignKey(p => p.CategoriaId);
+
+        }
+
+    }
+}
---
+++ ApiCatalogoMinimal/Program.cs
@@ -0,0 +1,29 @@
+using ApiCatalogoMinimal.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+//definir os endpoints
+
+app.MapGet("/", () => "Catálogo de Produtos 2023");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.Run();
+
