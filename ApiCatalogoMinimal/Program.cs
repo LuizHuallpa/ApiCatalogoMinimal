6ca9abfe8bad0d0578baa6f6e163d61f26090e65
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-02-09 20:41:14 -0300

    Adicionando Endpoints de produtos

--- ApiCatalogoMinimal/Program.cs
+++ ApiCatalogoMinimal/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connecti
 var app = builder.Build();
 //definir os endpoints
 
-app.MapGet("/", () => "Catálogo de Produtos 2023");
+app.MapGet("/", () => "Catálogo de Produtos 2023").ExcludeFromDescription();
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>{
     db.Categorias.Add(categoria);
@@ -69,6 +69,65 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//------------------------endpoints para Produto ---------------------------------
+app.MapPost("/produtos", async (Produto produto, AppDbContext db)
+ => {
+     db.Produtos.Add(produto);
+     await db.SaveChangesAsync();
+
+     return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+ });
+
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db)
+    => {
+        return await db.Produtos.FindAsync(id)
+                     is Produto produto
+                     ? Results.Ok(produto)
+                     : Results.NotFound();
+    });
+
+app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
+{
+
+    if (produto.ProdutoId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var produtoDB = await db.Produtos.FindAsync(id);
+
+    if (produtoDB is null) return Results.NotFound();
+
+    produtoDB.Nome = produto.Nome;
+    produtoDB.Descricao = produto.Descricao;
+    produtoDB.Preco = produto.Preco;
+    produtoDB.Imagem = produto.Imagem;
+    produtoDB.DataCompra = produto.DataCompra;
+    produtoDB.Estoque = produto.Estoque;
+    produtoDB.CategoriaId = produto.CategoriaId;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(produtoDB);
+});
+
+app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
+{
+    var produto = await db.Produtos.FindAsync(id);
+
+    if (produto is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Produtos.Remove(produto);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 
 
