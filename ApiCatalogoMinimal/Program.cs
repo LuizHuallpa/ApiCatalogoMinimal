3ef268413cc6607d951bac423f4594765dfb20b4
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-02-10 11:05:07 -0300

    Adicionando autenticação JWT

--- ApiCatalogoMinimal/Program.cs
+++ ApiCatalogoMinimal/Program.cs
@@ -1,6 +1,11 @@
-using ApiCatalogoMinimal.Context;
+﻿using ApiCatalogoMinimal.Context;
 using ApiCatalogoMinimal.Models;
+using ApiCatalogoMinimal.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +17,59 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddSingleton<ITokenService>(new TokenService());
+
+builder.Services.AddAuthentication
+                 (JwtBearerDefaults.AuthenticationScheme)
+                 .AddJwtBearer(options =>
+                 {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuer = true,
+                         ValidateAudience = true,
+                         ValidateLifetime = true,
+                         ValidateIssuerSigningKey = true,
+
+                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                         ValidAudience = builder.Configuration["Jwt:Audience"],
+                         IssuerSigningKey = new SymmetricSecurityKey
+                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                     };
+                 });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+//endpoint para login
+
+app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
+{
+    if (userModel == null)
+    {
+        return Results.BadRequest("Login Inv�lido");
+    }
+    if (userModel.UserName == "macoratti" && userModel.Password == "numsey#123")
+    {
+        var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+            app.Configuration["Jwt:Issuer"],
+            app.Configuration["Jwt:Audience"],
+            userModel);
+        return Results.Ok(new { token = tokenString });
+    }
+    else
+    {
+        return Results.BadRequest("Login Inv�lido");
+    }
+}).Produces(StatusCodes.Status400BadRequest)
+              .Produces(StatusCodes.Status200OK)
+              .WithName("Login")
+              .WithTags("Autenticacao");
+
+
 //definir os endpoints
 
-app.MapGet("/", () => "Cat�logo de Produtos 2023").ExcludeFromDescription();
+app.MapGet("/", () => "Catálogo de Produtos 2023").ExcludeFromDescription();
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>{
     db.Categorias.Add(categoria);
@@ -24,7 +78,7 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>{
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
-app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync()).RequireAuthorization();
 
 
 app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db)
@@ -79,7 +133,7 @@ app.MapPost("/produtos", async (Produto produto, AppDbContext db)
      return Results.Created($"/produtos/{produto.ProdutoId}", produto);
  });
 
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).RequireAuthorization();
 
 app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db)
     => {
@@ -129,7 +183,8 @@ app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
     return Results.NoContent();
 });
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
